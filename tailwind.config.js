c347047c194b37a4691b585b11f103dcd46321cc
mohammad javad majlesi <mohammadjavadmajlesi313@gmail.com>
2024-12-21 16:54:25 +0330

    create react project and install package lets go!

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor : {
+        "asli" : "#010211"
+      }
+    },
+  },
+  plugins: [],
+}
+
