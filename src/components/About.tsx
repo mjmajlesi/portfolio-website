daf288a293f201cece009234120e70763baaa988
mohammad javad majlesi <mohammadjavadmajlesi313@gmail.com>
2024-12-23 13:54:44 +0330

    complate about me component.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,10 @@
 import Container from "./Container";
 import imgabout from '../assets/download.svg';
 import styled from "styled-components";
+import { FaCss3, FaHtml5, FaReact } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io";
 
 const About = () => {
   return (
@@ -14,12 +18,46 @@ const About = () => {
             <image height="150" width="130" href={imgabout} />
           </StyleSvg>
         </div>
-        <div>
+        <div className=" my-20 flex justify-around items-center ">
           <div>
-
+            <img src={""} width={300} alt="imageBio" />
           </div>
           <div>
-
+            <p className="max-w-[700px] mb-10 font-medium text-[18px] leading-[26px]">
+            I am Highly motivated front-end developer with a strong desire to learn 
+            and grow. With 1 year of expertise in building websites and projects. 
+            I think the most important thing about me is that I am a fast learner
+            and flexible, which makes me fit well with any team.
+            * Also, in my current situation, I am more interested in learning and gaining
+            more experience, and the requested salary is not important to me
+            </p>
+            <div className="progress-bar">
+              <div className="flex pb-3 items-center gap-5">
+                <p className="min-w-[120px] font-medium text-lg">Html & Css</p>
+                <hr className="h-1 w-[75%] rounded-xl bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5]  " />
+              </div>
+              <div className="flex pb-3 items-center gap-5">
+                <p className="min-w-[120px] font-medium text-lg">Tailwind</p>
+                <hr className="h-1 w-[60%] rounded-xl bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5]  " />
+              </div>
+              <div className="flex pb-3 items-center gap-5">
+                <p className="min-w-[120px] font-medium text-lg">JavaScript</p>
+                <hr className="h-1 w-[70%] rounded-xl bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5]  " />
+              </div>
+              <div className="flex pb-5 items-center gap-5">
+                <p className="min-w-[120px] font-medium text-lg">ReactJs</p>
+                <hr className="h-1 w-[80%] rounded-xl bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5]  " />
+              </div>
+            </div>
+            <IconContext.Provider value={{ size : "50px"}}>
+            <div className="flex items-center gap-7 w-full justify-center mt-9">
+              <FaHtml5 />
+              <FaCss3 />
+              <RiTailwindCssFill />
+              <IoLogoJavascript />
+              <FaReact />
+            </div>
+            </IconContext.Provider>
           </div>
         </div>
       </Container>
