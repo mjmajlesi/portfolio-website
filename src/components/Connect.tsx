3824c5217bcd497dbb4db7e44ff68439dde4a07b
mohammad javad majlesi <mohammadjavadmajlesi313@gmail.com>
2024-12-24 14:50:06 +0330

    create connect component

---
+++ src/components/Connect.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Connect = () => {
+  return (
+    <div id='connect'>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Connect
