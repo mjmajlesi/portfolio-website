2c791601bb205d0565819c96853b85a25e92d914
mohammad javad majlesi <mohammadjavadmajlesi313@gmail.com>
2025-01-01 22:25:50 +0330

    create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import {Route , Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Container from "./Container";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import Button from "./buttuns";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const navs = [
@@ -23,7 +24,7 @@ function Navbar() {
     },
   ];
 
-  const [isNavOpen , setIsNavOpen] = useState(true);
+  const [isNavOpen, setIsNavOpen] = useState(true);
 
   const toggleNav = () => setIsNavOpen(!isNavOpen);
 
@@ -32,14 +33,19 @@ function Navbar() {
       <Container>
         <div className="flex flex-col items-start md:flex-row md:items-center md:justify-between mt-5 text-lg">
           <p className="font-semibold text-2xl ">MJ</p>
-          <div 
-          onClick={toggleNav}
-          className="absolute t0p-[0.75rem] right-[1rem] md:hidden flex flex-col justify-between w-[2.25rem] h-[2rem]">
+          <div
+            onClick={toggleNav}
+            className="absolute t0p-[0.75rem] right-[1rem] md:hidden flex flex-col justify-between w-[2.25rem] h-[2rem]"
+          >
             <span className="h-[0.4rem] w-full bg-white rounded-md"></span>
             <span className="h-[0.4rem] w-full bg-white rounded-md"></span>
             <span className="h-[0.4rem] w-full bg-white rounded-md"></span>
           </div>
-          <ul className={` ${ isNavOpen ? "hidden" : "flex" } md:flex flex-col md:flex-row gap-4 md:gap-10 items-center absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-5 md:p-0`}>
+          <ul
+            className={` ${
+              isNavOpen ? "hidden" : "flex"
+            } md:flex flex-col md:flex-row gap-4 md:gap-10 items-center absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto bg-gray-900 md:bg-transparent p-5 md:p-0`}
+          >
             {navs.map((nav, index) => (
               <li className="active:text-Blue">
                 <AnchorLink
@@ -48,13 +54,15 @@ function Navbar() {
                   offset={50}
                   href={nav.path}
                 >
-                  {nav.name}
+                  <Link to={"/"}>{nav.name}</Link>
                 </AnchorLink>
               </li>
             ))}
           </ul>
-          <div className="Login max-md:hidden" >
-            <Button className=" bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5] transition delay-150 hover:scale-110 ">Login</Button>
+          <div className="Login max-md:hidden">
+            <Button className=" bg-gradient-to-r from-[#1d1d9a] to-[#1e98d5] transition delay-150 hover:scale-110 ">
+              <Link to={"/login"}>Login</Link>
+            </Button>
           </div>
         </div>
       </Container>
